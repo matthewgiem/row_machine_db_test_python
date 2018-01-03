cec9b4fe954c88f5721775fa041219f7c1b15d7a
Matthew Giem <matthew.giem@gmail.com>
2018-01-02 22:41:17 -0800

    seed data base with one data point

--- row_times.py
+++ row_times.py
@@ -10,6 +10,14 @@ class RowTime(Model):
     class Meta:
         database = db
 
+row_times = [
+    {
+    'distance': 5000
+    'time': 23:15.6 # need to be converted
+    'date':  2/01/18 # need to be converted
+    }
+]
+
 if __name__ == '__main__':
     db.connect()
     db.create_tables([RowTime], safe=True)
